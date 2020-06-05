58c48899a3eb08db50822418eb4f2c4afc76cb11
Flavio Dantas <fdantasb@gmail.com>
2020-06-05 11:22:45 -0300

    Adicionada configuracao de eureka server

--- services/src/main/java/br/com/fdantasb/services/services/ServicesApplication.java
+++ services/src/main/java/br/com/fdantasb/services/services/ServicesApplication.java
@@ -2,8 +2,10 @@ package br.com.fdantasb.services.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServicesApplication {
 
 	public static void main(String[] args) {
