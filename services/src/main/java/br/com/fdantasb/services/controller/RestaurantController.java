34bd7660d12ef18fb5c91d4310d867980fc8512b
Flávio Dantas <fdantasb@gmail.com>
2020-06-20 14:59:46 -0300

    complementado o controller de restaurante com o metodo inicial

--- services/src/main/java/br/com/fdantasb/services/controller/RestaurantController.java
+++ services/src/main/java/br/com/fdantasb/services/controller/RestaurantController.java
@@ -1,4 +1,15 @@
 package br.com.fdantasb.services.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/restaurants")
 public class RestaurantController {
+
+    @RequestMapping
+    public ResponseEntity<String> restaurants(){
+        return ResponseEntity.ok("Hello World!");
+    }
 }
